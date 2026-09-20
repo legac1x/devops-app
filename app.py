1c948055a348bbf545dfbba590c6ec05cbe6bff4
Legac1x <yoourchanel@gmail.com>
2026-09-20 23:10:44 +0300

    fix: update returned value endpoint get_health

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ async def root():
 @app.get('/health')
 async def get_health():
     """Test dockstring get_health func"""
-    return {"status": "ok"}
+    return {"status": "broken"}
 
 @app.get('/version')
 async def test_stash():
